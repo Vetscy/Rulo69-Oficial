3e14be1e82c59ccc5b4f188034673f2a6af00ab9
Vetscy <contapessoaldovetscy@gmail.com>
2025-02-28 02:21:28 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// Este arquivo contém o código JavaScript para adicionar interatividade ao site. 
+// Você pode incluir funções para manipular o DOM, responder a eventos e realizar outras operações dinâmicas.
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Código para inicializar a interatividade do site
+    console.log('O site está pronto para interatividade!');
+});
